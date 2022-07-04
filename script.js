17769030330517f2caed639340e77dccc030501d
inheritance10 <ali552001a@gmail.com>
2022-07-05 00:49:02 +0300

    koltuk seçimi ve fiyatlanması duüzenlendi

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ const select = document.getElementById('movie');
 container.addEventListener('click',function (e){
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
         e.target.classList.toggle('selected');
-         let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-         let price = select.value;
-         amount.innerText = selectedSeatCount * price;
-         count.innerText = selectedSeatCount;
+        calculateTotal();
 
 
     }
 })
+
+
+select.addEventListener('change',function (e){
+   calculateTotal();
+});
+
+function calculateTotal(){
+    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+    let price = select.value;
+    amount.innerText = selectedSeatCount * price;
+    count.innerText = selectedSeatCount;
+}
 
 
